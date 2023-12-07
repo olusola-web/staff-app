3b5615288b634c777b4761087eb7271d74b3daf9
DESKTOP-IOMEBO6\user <oretugafolorunso@gmail.com>
2023-12-07 22:57:25 +0100

    table restructure

--- src/Components/PendingPurchReqHome/SingleHomePurchReq.js
+++ src/Components/PendingPurchReqHome/SingleHomePurchReq.js
@@ -4,7 +4,7 @@ const SingleHomePurchReq = ({ item, index }) => {
   // Determine the Tailwind class based on the status
   const statusClass =
     status === "Not granted"
-      ? "bg-red-500" // Red background for 'not granted'
+      ? "bg-red-500 " // Red background for 'not granted'
       : status === "Success"
       ? "bg-blue-500" // Blue background for 'Success'
       : "bg-green-500"; // Default background for other statuses
@@ -15,7 +15,7 @@ const SingleHomePurchReq = ({ item, index }) => {
       <div>{description}</div>
       <div>{quantity}</div>
       <div>{amount}</div>
-      <div className={`text-white ${statusClass} p-1 rounded`}>{status}</div>
+      <div className={`text-white ${statusClass} p-2 w-[4rem] rounded`}>{status}</div>
     </div>
   );
 };
--- src/Components/PurchaseRequest/PurchaseRequestTable.js
+++ src/Components/PurchaseRequest/PurchaseRequestTable.js
@@ -1,94 +1,64 @@
-import React, { useState, useEffect, useCallback } from "react";
-import DataTable from "../../Utils/TableReuse/TableDetails";
-import $ from 'jquery';
+import React, { useState } from 'react';
+import Table from '../../Utils/Table';
+import SinglePurchaseRequest from "./SinglePurchaseRequest";
+
+const tableHeader = [
+  "S/N",
+  "Description",
+  "Quantity",
+  "Amount",
+  "Action",
+];
 
 const PurchaseRequestTable = () => {
-  const initialData = [
+  const [displayedData, setDisplayedData] = useState([
     {
       id: 1,
-      Description: "I would like to get a printer that can be used to print out documents in the office",
-      Quantity: "1",
-      Amount: "#40000",
-      Action: "<button class='delete-btn' data-id='1'>Delete</button>"
+      description: "I would like to get a printer ",
+      quantity: "6",
+      amount: "₦40,000",
+      status: "Success",
     },
     {
       id: 2,
-      Description: "I need a new ergonomic office chair for prolonged working hours",
-      Quantity: "2",
-      Amount: "#15000",
-      Action: "<button class='delete-btn' data-id='2'>Delete</button>"
+      description: "Leave Req",
+      quantity: "1",
+      amount: "₦40,000",
+      status: "Pending",
     },
-    {
-      id: 3,
-      Description: "Request for high-quality HDMI cables for conference room setup",
-      Quantity: "5",
-      Amount: "#5000",
-      Action: "<button class='delete-btn' data-id='3'>Delete</button>"
-    }
-  ].map((item, index) => ({ ...item, serialNumber: index + 1 }));
+    // Add more data items as needed
+  ]);
 
-  const [data, setData] = useState(initialData);
-
-  const calculateTotals = () => {
-    let totalQuantity = 0;
-    let totalAmount = 0;
-
-    data.forEach(item => {
-      totalQuantity += parseInt(item.Quantity, 10);
-      totalAmount += parseInt(item.Amount.substring(1), 10); // Remove '#' and convert to number
-    });
-
-    return { totalQuantity, totalAmount };
+  const handleDelete = (index) => {
+    const newData = displayedData.filter((_, i) => i !== index);
+    setDisplayedData(newData);
   };
 
-  const { totalQuantity, totalAmount } = calculateTotals();
-
-  const handleDelete = useCallback((id) => {
-    const updatedData = data
-      .filter(item => item.id !== id)
-      .map((item, index) => ({ ...item, serialNumber: index + 1 }));
-    setData(updatedData);
-  }, [data]);
-
-  useEffect(() => {
-    const table = $(`#myTable`).DataTable({
-      // DataTables options here
-    });
-
-    $(`#myTable tbody`).on('click', 'button.delete-btn', function() {
-      const itemId = $(this).data('id');
-      handleDelete(itemId);
-    });
-
-    return () => {
-      if (table) {
-        table.destroy();
-      }
-    };
-  }, [data, handleDelete]);
-
-  const columns = [
-    { header: "Serial No", key: "serialNumber" },
-    { header: "Description", key: "Description" },
-    { header: "Quantity", key: "Quantity" },
-    { header: "Amount", key: "Amount" },
-    { header: "Action", key: "Action" }
-  ];
-
   return (
-    <div className="p-6">
-      <DataTable
-        data={data}
-        columns={columns}
-        options={{}} // Your DataTable options here
-      />
-
-      <div className="totals mt-2">
-        <p>Total Quantity: {totalQuantity}</p>
-        <p>Total Amount: #{totalAmount.toLocaleString()}</p>
+    <div>
+      <div className="w-full max-w-5xl mb-4 overflow-x-auto table-responsive">
+        <Table
+          headerContent={tableHeader}
+          minSize={"1000px"}
+          cols={5}
+          data={displayedData}
+          showSearch={false}
+          searchKey=""
+        >
+          {displayedData.map((item, index) => (
+            <div key={item.id}>
+              <SinglePurchaseRequest
+                item={item}
+                index={index}
+                onDelete={() => handleDelete(index)}
+              />
+              <hr className="my-4 border-green-50" />
+            </div>
+          ))}
+        </Table>
       </div>
     </div>
   );
-};
+}
 
 export default PurchaseRequestTable;
